6f994557f440345634f6ecb2d83caeb72745005c
wangjiping01 <wangjiping01@outlook.com>
2022-05-05 11:12:15 +0800

    modify

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 
 python main.py --items ./data/tmp.txt.3 --url ./resources/url.tmp.txt --output-dir ./outputs-tmp3
 
-python ./scripts/tmp.py merge1 a data/tmp.txt.2
+python ./scripts/tmp.py merge1 ./outputs-tmp3 data/tmp.txt.3
